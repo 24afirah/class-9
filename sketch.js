f9ed958631fcf3ed6021b775cd413976c09372cc
24afirah <123961878+24afirah@users.noreply.github.com>
2023-01-30 19:05:07 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,16 +1,42 @@
-console.log ("hello");
-var box
+
 function setup() {
   createCanvas(400,400);
-  box = createSprite (200,200,30,30);
+  background(51);
+  box = createSprite(200,200,30,30);
+
 }
 
 function draw() 
 {
-  background(30);
- drawSprites();
+
+  // write the code to change background color 
+  // to red when RIGHT_ARROW is pressed
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+   if (keyIsDown(RIGHT_ARROW))
+   {
+    background("red");
+   }
+
+
+  
+  drawSprites();
 }
-
-
-
 
